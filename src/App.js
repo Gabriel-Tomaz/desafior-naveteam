17248c171a76203a722d3cb9e3ed06d36a742c3f
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2021-03-03 21:22:26 -0300

    feat: validando tela de login e adicionando validacao as rotas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Default from './styles/default';
 import Routes from './routes';
+import {AuthProvider} from './Context/AuthContext';
 
 const App = () => {
   return(
-    <>
+    <AuthProvider>
       <Default />
       <Routes />
-    </>
+    </AuthProvider>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
+import Home from './pages/Home';
+
+import PrivateRoute from './components/PrivateRoute';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login}/>
+                <PrivateRoute isPrivate path="/" exact component={Home}/>
+                <Route path="/login" exact component={Login}/>
             </Switch>
         </BrowserRouter>
     );
